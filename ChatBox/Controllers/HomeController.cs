1c298e3aaa9e161860eb9e33e2309f24d4eac3b4
Nguyen Hoang <huyhoang121299@gmail.com>
2021-03-24 10:21:10 +0700

    Add project files.

---
+++ ChatBox/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using ChatBox.Models;
+using ChatBox.Models.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBox.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public IActionResult Login(string Username, string Password)
+        {
+            int i = 0;
+            using (SqlConnection con = new SqlConnection(Startup.connectionString))
+            {
+
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand("Add_User", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.Add("@UserName", SqlDbType.VarChar).Value = Username;
+                        cmd.Parameters.Add("@Password", SqlDbType.VarChar).Value = Multis.Multis.Encrypt(Password);
+
+                        con.Open();
+                        i = cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
+            return View();
+        }
+    }
+}
---
+++ ChatBox/wwwroot/js/User/User.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+
+
+
+});
+
+
+$("#btnlogin").click(function () {
+   
+    var Username = $("#Username").val();
+    var Password = $("#Password").val();
+    $.ajax({
+
+        url: "Home/Login",
+        type: 'POST',
+        data: { Username: Username, Password: Password },
+        success: function (result) {
+            $("#div1").html(result);
+        }
+        
+
+    });
+
+
+
+});
